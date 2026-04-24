689d3fccbc8d3b537f2800f98de62a03b6f1ab07
freelinex <freelinex7@gmail.com>
2026-04-24 18:40:48 +0300

    add task solution

---
+++ src/App copy.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import './App.scss';
+
+function getRandomName(): string {
+  const value = Date.now().toString().slice(-4);
+
+  return `Clock-${value}`;
+}
+
+export const App: React.FC = () => {
+  const today = new Date();
+  const [clockName, setClockName] = useState('Clock-0');
+
+  useEffect(() => {
+    const timerId = window.setInterval(() => {
+      setClockName(getRandomName());
+      // eslint-disable-next-line no-console
+      console.log(new Date().toUTCString().slice(-12, -4));
+    }, 1000);
+
+    return () => {
+      window.clearInterval(timerId);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>React clock</h1>
+
+      <div className="Clock">
+        <strong className="Clock__name">{clockName}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    </div>
+  );
+};
